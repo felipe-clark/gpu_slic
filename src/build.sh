550434fadde33b5b9ff8e9c7505fd3ccbd1262f8
Sander Pereira Clark  Felipe <sanderpe@ug68.eecg.toronto.edu>
2020-03-01 11:24:41 -0500

    Add sandbox .cu file and build shell script

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-nvcc slic.cu -arch=sm_52 -ccbin clang++-3.8 -o slic `pkg-config --libs opencv --cflags` $1 $2 $3 -Xptxas="-v"
+nvcc slic.cu -arch=sm_52 -ccbin clang++-3.8 -o slic.exe `pkg-config --libs opencv --cflags` $1 $2 $3 -Xptxas="-v"
--- src/slic.cu
+++ src/slic.cu
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 
-
 using namespace cv;
 
 int main(void)
