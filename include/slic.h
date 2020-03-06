6405ad30e1c6ccdf62fcdd575473ac14e614c9bd
Felipe Sander Pereira Clark <sanderpe@ug68.eecg.toronto.edu>
2020-03-06 18:36:49 -0500

    Adding forgotten header file updates

--- include/slic.h
+++ include/slic.h
@@ -1,6 +1,12 @@
 #ifndef __SLIC__
 #define __SLIC__
 
+#include <cmath>
+#include <limits>
+
+// Search window of superpixels
+const int window_size = 2;
+
 // Image size, measured in pixels
 const int pix_width = 4096;
 const int pix_height = 2048;
@@ -9,6 +15,15 @@ const int pix_height = 2048;
 const int spx_size = 64; //64 x 64
 const int spx_width = pix_width/spx_size;
 const int spx_height = pix_height/spx_size;
+
+const int slic_m = 10;
+const int slic_n = pix_width * pix_height;
+const int slic_k = spx_width * spx_height;
+const float slic_s = sqrt((float)slic_n/(float)slic_k);
+const float slic_factor_h = (float)slic_m / slic_s;
+const float max_float_h = std::numeric_limits<float>::max();
+__constant__ float slic_factor;
+__constant__ float max_float;
 
 // Pixel
 struct pix_data
@@ -49,12 +64,14 @@ void initialize_spx(spx_data* h_spx_data);
 // Kernels
 __global__ void k_cumulativeCount(const pix_data* d_pix_data, const own_data* d_own_data, spx_data* d_spx_data);
 __global__ void k_averaging(spx_data* d_spx_data);
+__global__ void k_ownership(const pix_data* d_pix_data, own_data* d_own_data, const spx_data* d_spx_data);
+__global__ void k_reset(spx_data* d_spx_data);
 // Test Kernels
 __global__ void k_test_darkenImage(const pix_data* d_pix_input, pix_data* d_pix_output);
 
 // Colorizers
 void color_solid(pix_data* h_pix_data, const own_data* h_own_data, const spx_data* h_spx_data);
-//void color_borders(pix_data* h_pix_data, const own_data* h_own_data, const spx_data* h_spx_data);
+void color_borders(pix_data* h_pix_data, const own_data* h_own_data, const spx_data* h_spx_data);
 // Test Colorizers
 void test_color_own(pix_data* h_pix_data, const own_data* h_own_data, const spx_data* h_spx_data);
 void test_color_spx(pix_data* h_pix_data, const own_data* h_own_data, const spx_data* h_spx_data);
