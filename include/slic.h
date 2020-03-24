bb54097bc08dbb3c534c3940999e48795d680714
Osovetsky  Evgeny <osovetsk@ug61.eecg.toronto.edu>
2020-03-24 15:13:32 -0400

    SMEM optimization for ownership kernel

--- include/slic.h
+++ include/slic.h
@@ -109,9 +109,10 @@ __global__ void k_cumulativeCountOpt1(const pix_data* d_pix_data, const own_data
 
 __global__ void k_averaging(spx_data* d_spx_data);
 
-#define k_ownership k_ownershipOpt
+#define k_ownership k_ownershipOpt2
 __global__ void k_ownershipOrig(const pix_data* d_pix_data, own_data* d_own_data, const spx_data* d_spx_data);
 __global__ void k_ownershipOpt(const pix_data* d_pix_data, own_data* d_own_data, const spx_data* d_spx_data);
+__global__ void k_ownershipOpt2(const pix_data* d_pix_data, own_data* d_own_data, const spx_data* d_spx_data);
 
 __global__ void k_reset(spx_data* d_spx_data);
 // Test Kernels
